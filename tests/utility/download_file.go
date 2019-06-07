7043bfec69334d785294b68f7f0e437b5f075acd
Umayanga <umayangag@gmail.com>
2019-06-07 16:23:45 +0530

    update unit test

--- tests/utility/download_file.go
+++ tests/utility/download_file.go
@@ -3,6 +3,7 @@ package utility
 import (
 	"GIG/app/utility"
 	"github.com/revel/revel/testing"
+	"os"
 )
 
 type DownloadFileTest struct {
@@ -14,6 +15,7 @@ func (t *DownloadFileTest) Before() {
 }
 
 func (t *DownloadFileTest) TestThatDownloadFileWorks() {
+	os.Remove("app/cache/downloadfiletest.ico")
 	link := "https://www.wikipedia.org/static/favicon/wikipedia.ico"
 	result := utility.DownloadFile("app/cache/downloadfiletest.ico",link)
 	t.AssertEqual(nil, result)
