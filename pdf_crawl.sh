2d78428a6172c8891b7e3135972cb6af3131987a
Umayanga <umayangag@gmail.com>
2019-07-08 15:56:25 +0530

    pdf crawler update

--- pdf_crawl.sh
+++ pdf_crawl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# go run app/utility/crawlers/pdf_crawler/pdf_crawler.go "http://www.buildings.gov.lk/index.php?option=com_content&view=category&layout=blog&id=47&Itemid=128&lang=en"
-# go run app/utility/crawlers/pdf_crawler/pdf_crawler.go "http://www.airforce.lk/main_tender.php"
-go run app/utility/crawlers/pdf_crawler/pdf_crawler.go "https://www.parliament.lk/gazettes"
+go run app/utility/crawlers/pdf_crawler/pdf_crawler.go "http://www.buildings.gov.lk/index.php?option=com_content&view=category&layout=blog&id=47&Itemid=128&lang=en"
+go run app/utility/crawlers/pdf_crawler/pdf_crawler.go "http://www.airforce.lk/main_tender.php"
+# go run app/utility/crawlers/pdf_crawler/pdf_crawler.go "https://www.parliament.lk/gazettes"
