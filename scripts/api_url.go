048436302cba0bc8db37742f190af09f88de5577
Umayanga <umayangag@gmail.com>
2019-09-10 14:32:54 +0530

    fix normalizer util

--- scripts/api_url.go
+++ scripts/api_url.go
@@ -3,6 +3,6 @@ package scripts
 /**
 Set the GIG server API url here for crawlers
  */
-var ApiUrl = "https://api.gigdemo.opensource.lk:9000/api/"
+var ApiUrl = "http://localhost:9000/api/"
 var NERServerUrl = "http://18.221.69.238:8080/classify"
 var NormalizeServer = ApiUrl + "normalize"
