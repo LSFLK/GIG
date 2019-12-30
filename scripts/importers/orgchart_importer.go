30f2eb22e3b344d3ee3dea87554d70222123d477
umayanga <umayangag@gmail.com>
2019-12-30 15:27:01 +0530

    save org chart to gig

--- scripts/importers/orgchart_importer.go
+++ scripts/importers/orgchart_importer.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"GIG/scripts/crawlers/pdf_crawler/create_entity"
 	"GIG/scripts/crawlers/pdf_crawler/parsers"
+	"GIG/scripts/crawlers/utils"
 	"flag"
 	"fmt"
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 /**
 config before running
  */
 
-var category = []string{"Gazette", "OrgChart"}
+var category = []string{"OrgChart"}
 
 func main() {
 	flag.Parse()
@@ -26,50 +29,81 @@ func main() {
 	textContent := parsers.ParsePdf(filePath) //parse pdf
 
 	fmt.Println("processing org chart info...")
-	splitPage:=strings.Split(textContent,parsers.NewPageMarker)
+	splitPage := strings.Split(textContent, parsers.NewPageMarker)
 
-	var ministryTitle1= regexp.MustCompile(`^\(\d+\) Minister of`)
-	var ministryTitle2= regexp.MustCompile(`^Minister of`)
+	var regexDate = regexp.MustCompile(`(0?[1-9]|[12][0-9]|3[01])*\.(0?[1-9]|1[012])\.\d{4}`)
+	var ministryTitle1 = regexp.MustCompile(`^\(\d+\) Minister of`)
+	var ministryTitle2 = regexp.MustCompile(`^Minister of`)
 
-	for _,page := range splitPage {
+	dateMatch := regexDate.FindStringSubmatch(textContent)
+	if len(dateMatch) > 0 {
+		gazetteDate, err := time.Parse("02.01.2006", dateMatch[0])
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(gazetteDate)
+	} else {
+		panic("Error: Unable to extract date of gazette")
+	}
+
+	dataStructure := make(map[string][]utils.NERResult)
+
+	for _, page := range splitPage {
 		splitArray := strings.Split(page, "\n")
 
 		for _, line := range splitArray {
 			ministryMatch1 := ministryTitle1.FindStringSubmatch(line)
 			ministryMatch2 := ministryTitle2.FindStringSubmatch(line)
 			if len(ministryMatch1) > 0 || len(ministryMatch2) > 0 {
-				fmt.Println(line)
+				ministryName := strings.TrimSpace(line)
+				ministryNameArray := strings.Split(line, ") ")
+				if len(ministryNameArray) == 2 {
+					ministryName = strings.TrimSpace(ministryNameArray[1])
+				}
 				i := 0
 				startDepartments := false
+				var departmentList []string
 				for {
 					if i == len(splitArray) {
-						fmt.Println("*****************")
 						break
 					}
 					subline := splitArray[i]
-					if len(subline) > 2 && (subline[0:2] == "* " || subline[0:2] == " (") { // where department list ends
+					if len(subline) > 2 && (subline[0:2] == "* " || subline[0:2] == " (" || subline[0:1] == "(") { // where department list ends
 						startDepartments = false
 					}
 					if startDepartments {
-						fmt.Println("	", subline)
+						if strings.Contains(subline, ". ") { // identify numbered line
+							departmentList = append(departmentList, subline)
+						} else {
+							index := len(departmentList) - 1
+							departmentList[index] = departmentList[index] + " " + subline
+						}
 					}
 					if subline == "Corporations" && splitArray[i+1][0:1] != "(" && strings.Contains(splitArray[i+1], ". ") { // where department list is assumed to start
 						startDepartments = true
 					}
+					//fmt.Println("		", subline)
 					i++
+				}
+
+				if len(departmentList) > 0 {
+					for _, listLine := range departmentList {
+						dataStructure[ministryName] = append(dataStructure[ministryName], utils.NERResult{Category: "Department", EntityName: strings.Split(listLine, ". ")[1]})
+					}
 				}
 
 			}
 		}
 	}
 
-	////NER extraction
-	//entityTitles, err := utils.ExtractEntityNames(textContent)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//if err := create_entity.CreateEntityFromText(filePath, "Gazette 2017", category, entityTitles); err != nil {
-	//	fmt.Println(err.Error(), filePath)
-	//}
+	for ministry, departments := range dataStructure {
+		fmt.Println(ministry)
+		for _, department := range departments {
+			fmt.Println("	", department.EntityName)
+		}
+		if err := create_entity.CreateEntityFromText("", ministry, category, departments); err != nil {
+			fmt.Println(err.Error(), filePath)
+		}
+	}
 
 }
