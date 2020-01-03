a02c0a32bf90ee7dc3f994474bc236cf040a8a6a
umayanga <umayangag@gmail.com>
2020-01-03 16:17:43 +0530

    remove search result limit

--- app/repositories/mongodb/entity_repository.go
+++ app/repositories/mongodb/entity_repository.go
@@ -93,12 +93,12 @@ func GetRelatedEntities(entity models.Entity) ([]models.Entity, error) {
 
 	if entity.Title != "" {
 		query["links"] = bson.M{"$in": []string{entity.Title}}
-		err = c.Session.Find(query).Sort("-_id").Limit(10).All(&entities)
+		err = c.Session.Find(query).Sort("-_id").All(&entities)
 
 		// if the entity is not of primitive type
 		if len(entities)==0{
 			query["links"] = bson.M{"$in": entity.Links}
-			err = c.Session.Find(query).Sort("-_id").Limit(10).All(&entities)
+			err = c.Session.Find(query).Sort("-_id").All(&entities)
 		}
 	}
 
