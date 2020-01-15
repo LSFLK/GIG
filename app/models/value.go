2284fa9f8128f3ebc74d63a1fc41843d61766829
umayanga <umayangag@gmail.com>
2020-01-15 14:36:37 +0530

    add source to value entity

--- app/models/value.go
+++ app/models/value.go
@@ -7,6 +7,7 @@ import (
 type Value struct {
 	Type      string    `json:"type" bson:"type"` // type can be string, date, json, wikiText, objectId, html, etc.
 	RawValue  string    `json:"raw_value" bson:"raw_value"`
+	Source    string    `json:"source" bson:"source"`
 	StartDate time.Time `json:"start_date" bson:"start_date"`
 	EndDate   time.Time `json:"end_date" bson:"end_date"`
 }
