3bad63110c6320c717a34435ec6571170d456a9c
umayanga <umayangag@gmail.com>
2020-01-17 11:32:01 +0530

    modify search result class

--- app/models/search_result.go
+++ app/models/search_result.go
@@ -5,22 +5,22 @@ import (
 )
 
 type SearchResult struct {
-	Title      string           `json:"title" bson:"title"`
-	Snippet    string           `json:"snippet" bson:"snippet"`
-	Categories []string         `json:"categories" bson:"categories"`
-	Attributes map[string]Value `json:"attributes" bson:"attributes"`
-	Links      []string         `json:"links" bson:"links"`
-	CreatedAt  time.Time        `json:"created_at" bson:"created_at"`
-	UpdatedAt  time.Time        `json:"updated_at" bson:"updated_at"`
-	ImageURL   string           `json:"image_url" bson:"image_url"`
+	Title      string             `json:"title" bson:"title"`
+	Snippet    string             `json:"snippet" bson:"snippet"`
+	Categories []string           `json:"categories" bson:"categories"`
+	Attributes map[string][]Value `json:"attributes" bson:"attributes"`
+	Links      []string           `json:"links" bson:"links"`
+	CreatedAt  time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt  time.Time          `json:"updated_at" bson:"updated_at"`
+	ImageURL   string             `json:"image_url" bson:"image_url"`
 }
 
 func (s SearchResult) ResultFrom(entity Entity) SearchResult {
 
-	var dataStructure = make(map[string]Value)
+	var dataStructure = make(map[string][]Value)
 
 	for _, attribute := range entity.Attributes {
-		dataStructure[attribute.Name] = attribute.GetValue()
+		dataStructure[attribute.Name] = attribute.Values
 	}
 
 	s.Title = entity.Title
