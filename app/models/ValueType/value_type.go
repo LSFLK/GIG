d79cb3cb75d9689aec61cb1c2f0c8b81ab25f79b
umayanga <umayangag@gmail.com>
2020-02-06 16:39:46 +0530

    fix issue #29 - add enums for value type

---
+++ app/models/ValueType/value_type.go
@@ -0,0 +1,10 @@
+package ValueType
+
+const (
+	String        = "string"
+	Date          = "date"
+	WikipediaText = "wikitext"
+	JSON          = "json"
+	ObjectId      = "objectId"
+	HTML          = "html"
+)
