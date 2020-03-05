54ba8b53ef388975250a963587c33a759e832221
umayanga <umayangag@gmail.com>
2020-03-05 11:15:11 +0530

    add source date to search result

--- app/models/search_result.go
+++ app/models/search_result.go
@@ -10,6 +10,7 @@ type SearchResult struct {
 	Categories []string           `json:"categories" bson:"categories"`
 	Attributes map[string][]Value `json:"attributes" bson:"attributes"`
 	Links      []string           `json:"links" bson:"links"`
+	SourceDate time.Time          `json:"source_date" bson:"source_date"`
 	CreatedAt  time.Time          `json:"created_at" bson:"created_at"`
 	UpdatedAt  time.Time          `json:"updated_at" bson:"updated_at"`
 	ImageURL   string             `json:"image_url" bson:"image_url"`
@@ -19,6 +20,7 @@ func (s SearchResult) ResultFrom(entity Entity) SearchResult {
 
 	s.Title = entity.GetTitle()
 	s.Snippet = entity.GetSnippet()
+	s.SourceDate = entity.GetSourceDate()
 	s.Categories = entity.GetCategories()
 	s.Attributes = entity.GetAttributes()
 	s.Links = entity.GetLinks()
