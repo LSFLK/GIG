87e61a36443707232fe70647b6f0bbd918bb0f3f
umayanga <umayangag@gmail.com>
2020-03-21 00:59:48 +0530

    tests for attribute model

--- app/models/attribute.go
+++ app/models/attribute.go
@@ -1,15 +1,24 @@
 package models
 
-import "sort"
+import (
+	"sort"
+	"time"
+)
 
 type Attribute struct {
 	Name   string  `json:"name" bson:"name"`
 	Values []Value `json:"values" bson:"values"`
 }
 
-func sortValues(Values []Value) []Value {
+func sortValues(Values []Value, order string) []Value {
 	values := Values
-	sort.Slice(values, func(i, j int) bool { return values[i].GetDate().Before(values[j].GetDate()) })
+	sort.Slice(values, func(i, j int) bool {
+		if order == "DESC" {
+			return values[i].GetDate().After(values[j].GetDate())
+		} else {
+			return values[i].GetDate().Before(values[j].GetDate())
+		}
+	})
 	return values
 }
 
@@ -26,7 +35,7 @@ func (a Attribute) GetName() string {
 Set New Value to Attribute
  */
 func (a Attribute) SetValue(value Value) Attribute {
-	a.Values = sortValues(append(a.Values, value))
+	a.Values = sortValues(append(a.Values, value), "ASC")
 	return a
 }
 
@@ -40,7 +49,24 @@ func (a Attribute) GetValue() Value {
 	return a.GetValues()[len(a.Values)-1]
 }
 
+/**
+Get Value of Attribute by date
+ */
+func (a Attribute) GetValueByDate(date time.Time) Value {
+	sortedValues := sortValues(a.Values, "DESC")
+
+	// pick the value with highest date lower than or equal to the given date
+	for _, value := range sortedValues {
+
+		if value.GetDate().Before(date) || value.GetDate().Equal(date) {
+			return value
+		}
+	}
+	//return the raw
+	return Value{}
+}
+
 func (a Attribute) GetValues() []Value {
-	a.Values = sortValues(a.Values)
+	a.Values = sortValues(a.Values, "ASC")
 	return a.Values
 }
--- tests/app/model/entity.go
+++ tests/app/model/entity.go
@@ -2,48 +2,7 @@ package model
 
 import (
 	"GIG/app/models"
-	"GIG/app/models/ValueType"
 	"strings"
-	"time"
-)
-
-var (
-	source0              = "source0"
-	source               = "source"
-	source2              = "source2"
-	source3              = "source3"
-	valueString          = "~test /tit?le % "
-	valueString2         = "~test /tit?le % 2"
-	valueString3         = "~test /tit?le % 3"
-	date, _              = time.Parse("2006-1-2", "2010-5-20")
-	date2, _             = time.Parse("2006-1-2", "2010-5-22")
-	date3, _             = time.Parse("2006-1-2", "2011-5-22")
-	valueType            = ValueType.String
-	formattedValueString = "2test -title"
-	testAttributeKey     = "test_attribute"
-
-	testValueObj0 = models.Value{}.
-		SetSource(source0).
-		SetValueString(valueString).
-		SetType(valueType)
-
-	testValueObj = models.Value{}.
-		SetSource(source).
-		SetValueString(valueString).
-		SetDate(date).
-		SetType(valueType)
-
-	testValueObj2 = models.Value{}.
-		SetSource(source2).
-		SetValueString(valueString2).
-		SetDate(date2).
-		SetType(valueType)
-
-	testValueObj3 = models.Value{}.
-		SetSource(source3).
-		SetValueString(valueString3).
-		SetDate(date3).
-		SetType(valueType)
 )
 
 /*
@@ -186,8 +145,6 @@ func (t *TestModels) TestThatEntitySetAttributeWorksForExistingAttributeWithSame
 	t.AssertEqual(testValue.GetSource(), source)
 	t.Assert(testValue.GetUpdatedDate().After(date))
 }
-
-//TODO: write function to get attribute value by date
 
 /*
 TODO: cases ignored (unlikely to occur if data is accurate)
