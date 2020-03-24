1c57b5ccc3e052604547171ad49376483c895bb8
umayanga <umayangag@gmail.com>
2020-03-24 18:22:06 +0530

    normalizer tests

--- tests/app/utility/normalizers/normalize.go
+++ tests/app/utility/normalizers/normalize.go
@@ -1,11 +1,33 @@
 package normalizers
 
 import (
+	"GIG/app/models"
+	"GIG/app/repositories"
 	"GIG/app/utilities/normalizers"
 )
 
-func (t *TestNormalizers) TestThatNormalizeWorks() {
+func (t *TestNormalizers) TestThatNormalizeAPIWorks() {
 	result, _ := normalizers.Normalize("sri lanka")
+	result2, _ := normalizers.Normalize("ranil")
+	result3, _ := normalizers.Normalize("sirisena sri lanka")
+	result4, _ := normalizers.Normalize("health ministry sri lanka")
+	result5, _ := normalizers.Normalize("election commission sri lanka")
 	t.AssertEqual(result, "Sri Lanka")
+	t.AssertEqual(result2, "Ranil Wickremesinghe")
+	t.AssertEqual(result3, "Maithripala Sirisena")
+	t.AssertEqual(result4, "Ministry of Health, Nutrition and Indigenous Medicine")
+	t.AssertEqual(result5, "Election Commission of Sri Lanka")
 
+}
+
+func (t *TestNormalizers) TestThatEntityNormalizerWorksWithNormalizerDatabase() {
+
+	entity := repositories.NormalizeEntityTitle(models.Entity{}.SetTitle(models.Value{
+		ValueString: "sri lanka",
+	}))
+	entity1 := repositories.NormalizeEntityTitle(models.Entity{}.SetTitle(models.Value{
+		ValueString: "ranil",
+	}))
+	t.AssertEqual(entity.GetTitle(), "Sri Lanka")
+	t.AssertEqual(entity1.GetTitle(), "Ranil Wickremesinghe")
 }
