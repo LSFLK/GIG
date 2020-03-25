44545fbdc22d6fdfc125267cf9e499928a854508
umayanga <umayangag@gmail.com>
2020-03-25 15:31:26 +0530

    modify normalizer tests

--- tests/app/utility/normalizers/normalize.go
+++ tests/app/utility/normalizers/normalize.go
@@ -22,13 +22,10 @@ func (t *TestNormalizers) TestThatNormalizeAPIWorks() {
 func (t *TestNormalizers) TestThatEntityNormalizerWorksWithNormalizerDatabase() {
 
 	result, err := repositories.NormalizeEntityTitle("sri lanka")
-	result2, err2 := repositories.NormalizeEntityTitle("ranil")
 	result3, err3 := repositories.NormalizeEntityTitle("All State Bank and their subsidiaries")
 
 	t.AssertEqual(result, "Sri Lanka")
 	t.AssertEqual(err, nil)
-	t.AssertEqual(result2, "Ranil Wickremesinghe")
-	t.AssertEqual(err2, nil)
 	t.AssertEqual(result3, "All State Bank and their subsidiaries")
 	t.AssertNotEqual(err3, nil)
 }
