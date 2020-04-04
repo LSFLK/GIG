58ccea75f38cb13a2907cb42cf38a9861f39f36e
umayanga <umayangag@gmail.com>
2020-04-04 19:08:43 +0530

    clean up code

--- app/utilities/normalizers/loader.go
+++ app/utilities/normalizers/loader.go
@@ -7,10 +7,5 @@ import (
 func LoadNormalizers() {
 	MapApiUrl, _ = revel.Config.String("normalizer.mapApiUrl")
 	MapAppKey, _ = revel.Config.String("normalizer.mapAppKey")
-
-	//SearchApiUrl, _ = revel.Config.String("normalizer.searchApiUrl")
-	//SearchAppKey, _ = revel.Config.String("normalizer.searchAppKey")
-	//Cx, _ = revel.Config.String("normalizer.cx")
-
 	StringMinMatchPercentage, _ = revel.Config.Int("normalizer.minMatchPercentage")
 }
