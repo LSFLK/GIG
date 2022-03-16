c137153f7211b5181d2a0d56efa9945b53ed35cb
umayanga <umayangag@gmail.com>
2022-03-16 16:26:41 +0530

    refactor pagination of get entity links

--- app/utilities/pagination/get_paginated_entity_links.go
+++ app/utilities/pagination/get_paginated_entity_links.go
@@ -13,25 +13,27 @@ func GetPaginatedEntityLinks(links []models.Link, attributesArray []string, page
 	if len(links) > offset {
 		for i, link := range links {
 			if i >= offset && i < upperLimit {
-				var (
-					linkedEntity models.Entity
-					err          error
-				)
-				if len(link.GetDates()) > 0 {
-					linkedEntity, err = repositories.EntityRepository{}.GetEntityByPreviousTitle(link.GetTitle(), link.GetDates()[0])
-				} else {
-					linkedEntity, err = repositories.EntityRepository{}.GetEntityBy("title", link.GetTitle())
-				}
-
-				if err != nil {
-					log.Println(link.GetTitle(), err)
-					return err, responseArray
-				} else {
-					responseArray = append(responseArray, models.SearchResult{}.ResultFrom(linkedEntity, attributesArray))
-				}
+				_, responseArray = addLinkedEntityToResults(link, responseArray, attributesArray)
 			}
 		}
 	}
 
 	return nil, responseArray
+}
+
+func addLinkedEntityToResults(link models.Link, responseArray []models.SearchResult, attributesArray []string) (error, []models.SearchResult) {
+	linkedEntity, err := getLinkedEntity(link)
+
+	if err != nil {
+		log.Println(link.GetTitle(), err)
+		return err, responseArray
+	}
+	return nil, append(responseArray, models.SearchResult{}.ResultFrom(linkedEntity, attributesArray))
+}
+
+func getLinkedEntity(link models.Link) (models.Entity, error) {
+	if len(link.GetDates()) > 0 {
+		return repositories.EntityRepository{}.GetEntityByPreviousTitle(link.GetTitle(), link.GetDates()[0])
+	}
+	return repositories.EntityRepository{}.GetEntityBy("title", link.GetTitle())
 }
