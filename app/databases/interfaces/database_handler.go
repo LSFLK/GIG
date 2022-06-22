e9e671ad34946637d8f7c0b49a97e2c1b8bd64f5
umayanga <umayangag@gmail.com>
2022-06-22 17:56:18 +0530

    remove interfaces

--- app/databases/interfaces/database_handler.go
+++
@@ -1,6 +0,0 @@
-package interfaces
-
-type DatabaseHandlerInterface interface {
-	GetServiceInstance() DatabaseServiceInterface
-	DisconnectService()
-}
--- app/databases/interfaces/database_service.go
+++
@@ -1,5 +0,0 @@
-package interfaces
-
-type DatabaseServiceInterface interface {
-	InitConnection(path string, dbName string, maxPool int)
-}
